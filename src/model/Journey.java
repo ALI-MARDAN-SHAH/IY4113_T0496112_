2de0d5c5a21de97350f76e56fc7de2b01877722b
HP <t0496112@my.ntu.ac.uk>
2026-05-16 23:35:40 +0100

    Started Journey model class

--- src/model/Journey.java
+++ src/model/Journey.java
@@ -1,4 +1,51 @@
 package model;
 
+import data.CityRideDataset;
+
 public class Journey {
+
+    private int id;
+    private String date;
+    private int fromZone;
+    private int toZone;
+    private CityRideDataset.TimeBand timeBand;
+    private CityRideDataset.PassengerType passengerType;
+
+    public Journey(int id, String date, int fromZone, int toZone,
+                   CityRideDataset.TimeBand timeBand,
+                   CityRideDataset.PassengerType passengerType) {
+        this.id = id;
+        this.date = date;
+        this.fromZone = fromZone;
+        this.toZone = toZone;
+        this.timeBand = timeBand;
+        this.passengerType = passengerType;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getFromZone() {
+        return fromZone;
+    }
+
+    public int getToZone() {
+        return toZone;
+    }
+
+    public int getZonesCrossed() {
+        return Math.abs(toZone - fromZone) + 1;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id
+                + " | Date: " + date
+                + " | From zone: " + fromZone
+                + " | To zone: " + toZone
+                + " | Time: " + timeBand
+                + " | Passenger: " + passengerType
+                + " | Zones crossed: " + getZonesCrossed();
+    }
 }
