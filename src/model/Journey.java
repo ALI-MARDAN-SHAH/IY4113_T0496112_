a580887af5a9fbf199421857cf542d14796fdc65
HP <t0496112@my.ntu.ac.uk>
2026-05-28 18:19:04 +0100

    Added fare calculation and journey charges
    Changes I Made:
    1 Added base fare, discount amount and charged fare to Journey
    2 Used CityRideDataset to get base fare and passenger discount rate
    3 Calculated the final charged fare when adding a journey
    4 Updated journey details to show fare information
    5 Tested adding and listing a journey with fare details

--- src/model/Journey.java
+++ src/model/Journey.java
@@ -10,16 +10,23 @@ public class Journey {
     private int toZone;
     private CityRideDataset.TimeBand timeBand;
     private CityRideDataset.PassengerType passengerType;
+    private double baseFare;
+    private double discountAmount;
+    private double chargedFare;
 
     public Journey(int id, String date, int fromZone, int toZone,
                    CityRideDataset.TimeBand timeBand,
-                   CityRideDataset.PassengerType passengerType) {
+                   CityRideDataset.PassengerType passengerType,
+                   double baseFare, double discountAmount, double chargedFare) {
         this.id = id;
         this.date = date;
         this.fromZone = fromZone;
         this.toZone = toZone;
         this.timeBand = timeBand;
         this.passengerType = passengerType;
+        this.baseFare = baseFare;
+        this.discountAmount = discountAmount;
+        this.chargedFare = chargedFare;
     }
 
     public int getId() {
@@ -34,9 +41,34 @@ public class Journey {
         return toZone;
     }
 
+    public CityRideDataset.PassengerType getPassengerType() {
+        return passengerType;
+    }
+
+    public CityRideDataset.TimeBand getTimeBand() {
+        return timeBand;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public double getBaseFare() {
+        return baseFare;
+    }
+
+    public double getDiscountAmount() {
+        return discountAmount;
+    }
+
+    public double getChargedFare() {
+        return chargedFare;
+    }
+
     public int getZonesCrossed() {
         return Math.abs(toZone - fromZone) + 1;
     }
+
     public String showJourneyDetails() {
         return "ID: " + id
                 + " | Date: " + date
@@ -44,6 +76,9 @@ public class Journey {
                 + " | To zone: " + toZone
                 + " | Time: " + timeBand
                 + " | Passenger: " + passengerType
-                + " | Zones crossed: " + getZonesCrossed();
+                + " | Zones crossed: " + getZonesCrossed()
+                + " | Base fare: £" + String.format("%.2f", baseFare)
+                + " | Discount: £" + String.format("%.2f", discountAmount)
+                + " | Charged fare: £" + String.format("%.2f", chargedFare);
     }
 }
--- src/service/CityRideService.java
+++ src/service/CityRideService.java
@@ -19,19 +19,27 @@ public class CityRideService {
     public void addJourney(String date, int fromZone, int toZone,
                            CityRideDataset.TimeBand timeBand,
                            CityRideDataset.PassengerType passengerType) {
-        Journey journey = new Journey(nextJourneyId, date, fromZone, toZone, timeBand, passengerType);
+        double baseFare = CityRideDataset.getBaseFare(fromZone, toZone, timeBand).doubleValue();
+        double discountRate = CityRideDataset.DISCOUNT_RATE.get(passengerType).doubleValue();
+        double discountAmount = baseFare * discountRate;
+        double chargedFare = baseFare - discountAmount;
+
+        Journey journey = new Journey(nextJourneyId, date, fromZone, toZone,
+                timeBand, passengerType, baseFare, discountAmount, chargedFare);
 
         journeys.add(journey);
         nextJourneyId++;
 
         System.out.println("Journey added.");
+        System.out.println("Fare charged: £" + String.format("%.2f", chargedFare));
     }
 
     public void showAllJourneys() {
         if (journeys.size() == 0) {
-            System.out.println("No journeys added yet");
+            System.out.println("No journeys added yet.");
             return;
         }
+
         for (Journey journey : journeys) {
             System.out.println(journey.showJourneyDetails());
         }
